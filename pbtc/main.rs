0f73927e516c4134f2d58853cfab9651d61418a6
debris <marek.kotewicz@gmail.com>
2016-09-30 14:59:16 +0200

    added testnet and seednode cli options

--- pbtc/main.rs
+++ pbtc/main.rs
@@ -8,12 +8,25 @@ extern crate chain;
 extern crate keys;
 extern crate primitives;
 extern crate script;
+extern crate net;
+extern crate p2p;
+
+use net::common::Magic;
 
 fn main() {
 	let yaml = load_yaml!("cli.yml");
 	let matches = clap::App::from_yaml(yaml).get_matches();
 
+	let _magic = match matches.is_present("testnet") {
+		true => Magic::Testnet,
+		false => Magic::Mainnet,
+	};
+
 	if let Some(ip) = matches.value_of("connect") {
 		println!("Connecting to ip: {}", ip);
+	}
+
+	if let Some(ip) = matches.value_of("seednode") {
+		println!("Seednode ip: {}", ip);
 	}
 }
