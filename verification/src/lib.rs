2991cc619355b158caf2fec96067898399894c42
NikVolf <nikvolf@gmail.com>
2016-10-17 22:27:45 +0300

    crate layout

---
+++ verification/src/lib.rs
@@ -0,0 +1,11 @@
+//! Bitcoin blocks verification
+
+extern crate db;
+extern crate primitives;
+extern crate chain;
+extern crate serialization;
+extern crate parking_lot;
+
+#[cfg(test)]
+extern crate ethcore_devtools as devtools;
+
