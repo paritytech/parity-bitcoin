543d66ce03e11c969f5bb27aef5d3eb2e3b1c797
Svyatoslav Nikolsky <svyatonik@gmail.com>
2016-10-31 13:18:58 +0300

    Fixed grumbles from #54 (#61)
    * async verifying
    
    * lost file
    
    * removed debug timer
    
    * fixed compilation && tests
    
    * fixed typo
    
    * async verification refactoring
    
    * fixed sync stall
    
    * fixed dead_code warning
    
    * get rid of extra locks
    
    * fixed tests
    
    * * separated tasks executor from sync node
    * local node mutex removed
    
    * fixed grumbles from #54

--- sync/src/inbound_connection.rs
+++ sync/src/inbound_connection.rs
@@ -17,79 +17,79 @@ impl InboundConnection {
 }
 
 impl InboundSyncConnection for InboundConnection {
-	fn start_sync_session(&mut self, version: u32) {
+	fn start_sync_session(&self, version: u32) {
 		self.local_node.start_sync_session(self.peer_index, version);
 	}
 
-	fn on_inventory(&mut self, message: types::Inv) {
+	fn on_inventory(&self, message: types::Inv) {
 		self.local_node.on_peer_inventory(self.peer_index, message);
 	}
 
-	fn on_getdata(&mut self, message: types::GetData) {
+	fn on_getdata(&self, message: types::GetData) {
 		self.local_node.on_peer_getdata(self.peer_index, message);
 	}
 
-	fn on_getblocks(&mut self, message: types::GetBlocks) {
+	fn on_getblocks(&self, message: types::GetBlocks) {
 		self.local_node.on_peer_getblocks(self.peer_index, message);
 	}
 
-	fn on_getheaders(&mut self, message: types::GetHeaders) {
+	fn on_getheaders(&self, message: types::GetHeaders) {
 		self.local_node.on_peer_getheaders(self.peer_index, message);
 	}
 
-	fn on_transaction(&mut self, message: types::Tx) {
+	fn on_transaction(&self, message: types::Tx) {
 		self.local_node.on_peer_transaction(self.peer_index, message);
 	}
 
-	fn on_block(&mut self, message: types::Block) {
+	fn on_block(&self, message: types::Block) {
 		self.local_node.on_peer_block(self.peer_index, message);
 	}
 
-	fn on_headers(&mut self, message: types::Headers) {
+	fn on_headers(&self, message: types::Headers) {
 		self.local_node.on_peer_headers(self.peer_index, message);
 	}
 
-	fn on_mempool(&mut self, message: types::MemPool) {
+	fn on_mempool(&self, message: types::MemPool) {
 		self.local_node.on_peer_mempool(self.peer_index, message);
 	}
 
-	fn on_filterload(&mut self, message: types::FilterLoad) {
+	fn on_filterload(&self, message: types::FilterLoad) {
 		self.local_node.on_peer_filterload(self.peer_index, message);
 	}
 
-	fn on_filteradd(&mut self, message: types::FilterAdd) {
+	fn on_filteradd(&self, message: types::FilterAdd) {
 		self.local_node.on_peer_filteradd(self.peer_index, message);
 	}
 
-	fn on_filterclear(&mut self, message: types::FilterClear) {
+	fn on_filterclear(&self, message: types::FilterClear) {
 		self.local_node.on_peer_filterclear(self.peer_index, message);
 	}
 
-	fn on_merkleblock(&mut self, message: types::MerkleBlock) {
+	fn on_merkleblock(&self, message: types::MerkleBlock) {
 		self.local_node.on_peer_merkleblock(self.peer_index, message);
 	}
 
-	fn on_sendheaders(&mut self, message: types::SendHeaders) {
+	fn on_sendheaders(&self, message: types::SendHeaders) {
 		self.local_node.on_peer_sendheaders(self.peer_index, message);
 	}
 
-	fn on_feefilter(&mut self, message: types::FeeFilter) {
+	fn on_feefilter(&self, message: types::FeeFilter) {
 		self.local_node.on_peer_feefilter(self.peer_index, message);
 	}
 
-	fn on_send_compact(&mut self, message: types::SendCompact) {
+	fn on_send_compact(&self, message: types::SendCompact) {
 		self.local_node.on_peer_send_compact(self.peer_index, message);
 	}
 
-	fn on_compact_block(&mut self, message: types::CompactBlock) {
+	fn on_compact_block(&self, message: types::CompactBlock) {
 		self.local_node.on_peer_compact_block(self.peer_index, message);
 	}
 
-	fn on_get_block_txn(&mut self, message: types::GetBlockTxn) {
+	fn on_get_block_txn(&self, message: types::GetBlockTxn) {
 		self.local_node.on_peer_get_block_txn(self.peer_index, message);
 	}
 
-	fn on_block_txn(&mut self, message: types::BlockTxn) {
+	fn on_block_txn(&self, message: types::BlockTxn) {
 		self.local_node.on_peer_block_txn(self.peer_index, message);
 	}
 }
