5fcba908f4a8d364fada9edd278a36aa225fad77
NikVolf <nikvolf@gmail.com>
2016-11-14 21:17:54 +0300

    transaction & block sigops amount check

---
+++ devtools/src/test_logger.rs
@@ -0,0 +1,22 @@
+use log::{self, Log, LogRecord, LogLevel, LogMetadata, SetLoggerError, LogLevelFilter};
+
+struct TestLogger;
+
+impl Log for TestLogger {
+    fn enabled(&self, metadata: &LogMetadata) -> bool {
+        metadata.level() <= LogLevel::Info
+    }
+
+    fn log(&self, record: &LogRecord) {
+        if self.enabled(record.metadata()) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+}
+
+pub fn init() -> Result<(), SetLoggerError> {
+    log::set_logger(|max_log_level| {
+        max_log_level.set(LogLevelFilter::Info);
+        Box::new(TestLogger)
+    })
+}
