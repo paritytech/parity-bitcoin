231604ea493554550bb756ebc408271ed6e070a0
NikVolf <nikvolf@gmail.com>
2016-11-15 23:23:01 +0300

    remove print

--- verification/src/chain_verifier.rs
+++ verification/src/chain_verifier.rs
@@ -289,9 +289,7 @@ mod tests {
 	fn verify_smoky() {
 		let storage = TestStorage::with_blocks(&vec![test_data::genesis()]);
 		let b1 = test_data::block_h1();
-
-		println!("{:?}", b1.transactions()[0].inputs[0].script_sig());
-
+		
 		let verifier = ChainVerifier::new(Arc::new(storage));
 		assert_eq!(Chain::Main, verifier.verify(&b1).unwrap());
 	}
