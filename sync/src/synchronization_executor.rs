edbdf4fb7ef5c0b232be16011933fb0d2ce7e904
debris <marek.kotewicz@gmail.com>
2016-11-21 17:56:09 +0100

    revert "relay canonized blocks"
    This reverts commit 0d8bfc8e574bfd7aca4efe87f8d9fb6a18fe5c14.

--- sync/src/synchronization_executor.rs
+++ sync/src/synchronization_executor.rs
@@ -30,8 +30,6 @@ pub enum Task {
 	RequestMemoryPool(usize),
 	/// Send block.
 	SendBlock(usize, Block, ServerTaskIndex),
-	/// Broadcast block.
-	BroadcastBlocksHashes(Vec<H256>),
 	/// Send notfound
 	SendNotFound(usize, Vec<InventoryVector>, ServerTaskIndex),
 	/// Send inventory
@@ -132,20 +130,6 @@ impl TaskExecutor for LocalSynchronizationTaskExecutor {
 					assert_eq!(id.raw(), None);
 					trace!(target: "sync", "Sending block {:?} to peer#{}", block_message.block.hash(), peer_index);
 					connection.send_block(&block_message);
-				}
-			},
-			Task::BroadcastBlocksHashes(blocks_hashes) => {
-				let inventory = types::Inv {
-						inventory: blocks_hashes.into_iter().map(|h| InventoryVector {
-							inv_type: InventoryType::MessageBlock,
-							hash: h,
-						})
-						.collect(),
-				};
-
-				for (peer_index, connection) in self.peers.iter() {
-					trace!(target: "sync", "Sending inventory with {} blocks hashes to peer#{}", inventory.inventory.len(), peer_index);
-					connection.send_inventory(&inventory);
 				}
 			},
 			Task::SendNotFound(peer_index, unknown_inventory, id) => {
