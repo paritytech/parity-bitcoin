6c7f2ed1aecd38dccfc99ab724bc72b8d221a45e
NikVolf <nikvolf@gmail.com>
2016-11-22 17:09:20 +0300

    uint from parity

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -3,5 +3,10 @@ extern crate rustc_serialize;
 
 pub mod bytes;
 pub mod hash;
+pub mod uint;
 
 pub use rustc_serialize::hex;
+
+pub use uint::U256;
+pub use hash::{H160, H256};
+pub use bytes::Bytes;
