dcd55557f63481db7e16d3efca63d0297f2fc520
NikVolf <nikvolf@gmail.com>
2016-12-01 19:36:32 +0300

    use intermediate file

--- tools/regtests.sh
+++ tools/regtests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ./target/release/pbtc --regtest &
-! java -jar ./tools/test-scripts/pull-tests-f56eec3.jar /tmp/regtest-db 2>&1 | grep -E 'org.bitcoinj.store.BlockStoreException\:|BitcoindComparisonTool.main\: ERROR'
+java -jar ./tools/test-scripts/pull-tests-f56eec3.jar /tmp/regtest-db 2>&1 | tee regtests.log
+! grep -E --color=auto 'org.bitcoinj.store.BlockStoreException\:|BitcoindComparisonTool.main\: ERROR|bitcoind sent us a block it already had, make sure bitcoind has no blocks!' regtests.log
 result=$?
 pkill -f ./target/release/pbtc
+rm regtests.log
 exit "$result"
