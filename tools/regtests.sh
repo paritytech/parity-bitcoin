159d6cd0b8e6a3c758c8fd74b07e9ad907981b1b
NikVolf <nikvolf@gmail.com>
2016-12-01 23:47:41 +0300

    more convinient log output

--- tools/regtests.sh
+++ tools/regtests.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 ./target/release/pbtc --regtest --db-cache=192 &
-! java -jar ./tools/compare-tool/pull-tests-be0eef7.jar /tmp/regtest-db 2>&1 | grep -E --color=auto 'org.bitcoinj.store.BlockStoreException\:|BitcoindComparisonTool.main\: ERROR|bitcoind sent us a block it already had, make sure bitcoind has no blocks!|java.lang.NullPointerException'
+! java -jar ./tools/compare-tool/pull-tests-be0eef7.jar /tmp/regtest-db 2>&1 | tee regtests-full.log | grep -E --color=auto 'org.bitcoinj.store.BlockStoreException\:|BitcoindComparisonTool.main\: ERROR|bitcoind sent us a block it already had, make sure bitcoind has no blocks!|java.lang.NullPointerException'
 result=$?
+
+if [ $result -eq 1 ]
+then
+  echo "Regtests failed" | grep --color=auto "failed"
+  echo "-----------------------------"
+  echo "Full log: "
+  cat regtests-full.log
+else
+  echo "Reg tests ok, test cases: "
+  GREP_COLOR="01;32" grep -E "BitcoindComparisonTool.main: Block \"b[0-9]*\" completed processing" regtests-full.log
+fi
+
 pkill -f ./target/release/pbtc
 exit "$result"
