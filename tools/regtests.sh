462baee57148756399be3daf4d4b796d5956b15a
NikVolf <nikvolf@gmail.com>
2016-12-01 23:10:07 +0300

    updating tool

--- tools/regtests.sh
+++ tools/regtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./target/release/pbtc --regtest --db-cache=192 &
-! java -jar ./tools/test-scripts/pull-tests-f56eec3.jar /tmp/regtest-db 2>&1 | grep -E --color=auto 'org.bitcoinj.store.BlockStoreException\:|BitcoindComparisonTool.main\: ERROR|bitcoind sent us a block it already had, make sure bitcoind has no blocks!|java.lang.NullPointerException'
+! java -jar ./tools/compare-tool/pull-tests-be0eef7.jar /tmp/regtest-db 2>&1 | grep -E --color=auto 'org.bitcoinj.store.BlockStoreException\:|BitcoindComparisonTool.main\: ERROR|bitcoind sent us a block it already had, make sure bitcoind has no blocks!|java.lang.NullPointerException'
 result=$?
 pkill -f ./target/release/pbtc
 exit "$result"
