12280b398bdfa7663d0763fe802d4a02cf57df5a
NikVolf <nikvolf@gmail.com>
2016-12-01 20:06:42 +0300

    use minimum cache

--- tools/regtests.sh
+++ tools/regtests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./target/release/pbtc --regtest &
+./target/release/pbtc --regtest --db-cache=192 &
 java -jar ./tools/test-scripts/pull-tests-f56eec3.jar /tmp/regtest-db 2>&1 | tee regtests.log
 ! grep -E --color=auto 'org.bitcoinj.store.BlockStoreException\:|BitcoindComparisonTool.main\: ERROR|bitcoind sent us a block it already had, make sure bitcoind has no blocks!' regtests.log
 result=$?
