f5d2cdfbce540db9ae9f5a6082a09e8152674c3d
Svyatoslav Nikolsky <svyatonik@gmail.com>
2016-12-09 12:49:18 +0300

    added ser/deser tests for BlockTemplate

--- rpc/src/v1/types/block_template.rs
+++ rpc/src/v1/types/block_template.rs
@@ -15,7 +15,7 @@ pub struct BlockTemplate {
 	/// The preferred block version
 	pub version: u32,
 	/// Specific block rules that are to be enforced
-	pub rules: Vec<String>,
+	pub rules: Option<Vec<String>>,
 	/// Set of pending, supported versionbit (BIP 9) softfork deployments
 	/// Keys: named softfork rules
 	/// Values: identifies the bit number as indicating acceptance and readiness for given key
@@ -137,6 +137,138 @@ mod tests {
 				sigops: Some(200),
 				weight: Some(300),
 				required: true,
+			});
+	}
+
+	#[test]
+	fn block_template_serialize() {
+		assert_eq!(serde_json::to_string(&BlockTemplate {
+			version: 0,
+			rules: None,
+			vbavailable: None,
+			vbrequired: None,
+			previousblockhash: H256::default(),
+			transactions: vec![],
+			coinbaseaux: None,
+			coinbasevalue: None,
+			coinbasetxn: None,
+			target: H256::default(),
+			mintime: None,
+			mutable: None,
+			noncerange: None,
+			sigoplimit: None,
+			sizelimit: None,
+			weightlimit: None,
+			curtime: 100,
+			bits: 200,
+			height: 300,
+		}).unwrap(), r#"{"version":0,"rules":null,"vbavailable":null,"vbrequired":null,"previousblockhash":"0000000000000000000000000000000000000000000000000000000000000000","transactions":[],"coinbaseaux":null,"coinbasevalue":null,"coinbasetxn":null,"target":"0000000000000000000000000000000000000000000000000000000000000000","mintime":null,"mutable":null,"noncerange":null,"sigoplimit":null,"sizelimit":null,"weightlimit":null,"curtime":100,"bits":200,"height":300}"#);
+		assert_eq!(serde_json::to_string(&BlockTemplate {
+			version: 0,
+			rules: Some(vec!["a".to_owned()]),
+			vbavailable: Some(vec![("b".to_owned(), 5)].into_iter().collect()),
+			vbrequired: Some(10),
+			previousblockhash: H256::from(10),
+			transactions: vec![BlockTemplateTransaction {
+				data: Bytes("00010203".from_hex().unwrap()),
+				txid: None,
+				hash: None,
+				depends: None,
+				fee: None,
+				sigops: None,
+				weight: None,
+				required: false,
+			}],
+			coinbaseaux: Some(vec![("c".to_owned(), "d".to_owned())].into_iter().collect()),
+			coinbasevalue: Some(30),
+			coinbasetxn: Some(BlockTemplateTransaction {
+				data: Bytes("555555".from_hex().unwrap()),
+				txid: Some(H256::from(44)),
+				hash: Some(H256::from(55)),
+				depends: Some(vec![1]),
+				fee: Some(300),
+				sigops: Some(400),
+				weight: Some(500),
+				required: true,
+			}),
+			target: H256::from(100),
+			mintime: Some(7),
+			mutable: Some(vec!["afg".to_owned()]),
+			noncerange: Some("00000000ffffffff".to_owned()),
+			sigoplimit: Some(45),
+			sizelimit: Some(449),
+			weightlimit: Some(523),
+			curtime: 100,
+			bits: 200,
+			height: 300,
+		}).unwrap(), r#"{"version":0,"rules":["a"],"vbavailable":{"b":5},"vbrequired":10,"previousblockhash":"0a00000000000000000000000000000000000000000000000000000000000000","transactions":[{"data":"00010203","txid":null,"hash":null,"depends":null,"fee":null,"sigops":null,"weight":null,"required":false}],"coinbaseaux":{"c":"d"},"coinbasevalue":30,"coinbasetxn":{"data":"555555","txid":"2c00000000000000000000000000000000000000000000000000000000000000","hash":"3700000000000000000000000000000000000000000000000000000000000000","depends":[1],"fee":300,"sigops":400,"weight":500,"required":true},"target":"6400000000000000000000000000000000000000000000000000000000000000","mintime":7,"mutable":["afg"],"noncerange":"00000000ffffffff","sigoplimit":45,"sizelimit":449,"weightlimit":523,"curtime":100,"bits":200,"height":300}"#);
+	}
+
+	#[test]
+	fn block_template_deserialize() {
+		assert_eq!(
+			serde_json::from_str::<BlockTemplate>(r#"{"version":0,"rules":null,"vbavailable":null,"vbrequired":null,"previousblockhash":"0000000000000000000000000000000000000000000000000000000000000000","transactions":[],"coinbaseaux":null,"coinbasevalue":null,"coinbasetxn":null,"target":"0000000000000000000000000000000000000000000000000000000000000000","mintime":null,"mutable":null,"noncerange":null,"sigoplimit":null,"sizelimit":null,"weightlimit":null,"curtime":100,"bits":200,"height":300}"#).unwrap(),
+			BlockTemplate {
+				version: 0,
+				rules: None,
+				vbavailable: None,
+				vbrequired: None,
+				previousblockhash: H256::default(),
+				transactions: vec![],
+				coinbaseaux: None,
+				coinbasevalue: None,
+				coinbasetxn: None,
+				target: H256::default(),
+				mintime: None,
+				mutable: None,
+				noncerange: None,
+				sigoplimit: None,
+				sizelimit: None,
+				weightlimit: None,
+				curtime: 100,
+				bits: 200,
+				height: 300,
+			});
+		assert_eq!(
+			serde_json::from_str::<BlockTemplate>(r#"{"version":0,"rules":["a"],"vbavailable":{"b":5},"vbrequired":10,"previousblockhash":"0a00000000000000000000000000000000000000000000000000000000000000","transactions":[{"data":"00010203","txid":null,"hash":null,"depends":null,"fee":null,"sigops":null,"weight":null,"required":false}],"coinbaseaux":{"c":"d"},"coinbasevalue":30,"coinbasetxn":{"data":"555555","txid":"2c00000000000000000000000000000000000000000000000000000000000000","hash":"3700000000000000000000000000000000000000000000000000000000000000","depends":[1],"fee":300,"sigops":400,"weight":500,"required":true},"target":"6400000000000000000000000000000000000000000000000000000000000000","mintime":7,"mutable":["afg"],"noncerange":"00000000ffffffff","sigoplimit":45,"sizelimit":449,"weightlimit":523,"curtime":100,"bits":200,"height":300}"#).unwrap(),
+			BlockTemplate {
+				version: 0,
+				rules: Some(vec!["a".to_owned()]),
+				vbavailable: Some(vec![("b".to_owned(), 5)].into_iter().collect()),
+				vbrequired: Some(10),
+				previousblockhash: H256::from(10),
+				transactions: vec![BlockTemplateTransaction {
+					data: Bytes("00010203".from_hex().unwrap()),
+					txid: None,
+					hash: None,
+					depends: None,
+					fee: None,
+					sigops: None,
+					weight: None,
+					required: false,
+				}],
+				coinbaseaux: Some(vec![("c".to_owned(), "d".to_owned())].into_iter().collect()),
+				coinbasevalue: Some(30),
+				coinbasetxn: Some(BlockTemplateTransaction {
+					data: Bytes("555555".from_hex().unwrap()),
+					txid: Some(H256::from(44)),
+					hash: Some(H256::from(55)),
+					depends: Some(vec![1]),
+					fee: Some(300),
+					sigops: Some(400),
+					weight: Some(500),
+					required: true,
+				}),
+				target: H256::from(100),
+				mintime: Some(7),
+				mutable: Some(vec!["afg".to_owned()]),
+				noncerange: Some("00000000ffffffff".to_owned()),
+				sigoplimit: Some(45),
+				sizelimit: Some(449),
+				weightlimit: Some(523),
+				curtime: 100,
+				bits: 200,
+				height: 300,
 			});
 	}
 }
