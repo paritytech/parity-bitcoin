1930d8348595caa782568c93577b9e65b24683cd
NikVolf <nikvolf@gmail.com>
2016-12-13 11:44:06 +0100

    rpc method stub

--- rpc/src/v1/traits/network.rs
+++ rpc/src/v1/traits/network.rs
@@ -10,5 +10,10 @@ build_rpc_trait! {
 		/// @curl-example: curl --data-binary '{"jsonrpc": "2.0", "method": "addnode", "params": ["127.0.0.1:8888", "onetry"], "id":1 }' -H 'content-type: application/json;' http://127.0.0.1:8332/
 		#[rpc(name = "addnode")]
 		fn add_node(&self, String, AddNodeOperation) -> Result<(), Error>;
+		/// Query node(s) info
+		/// @curl-example: curl --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getaddednodeinfo", "params": [true, "192.168.0.201"] }' -H 'content-type: application/json;' http://127.0.0.1:8332/
+		/// @curl-example: curl --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getaddednodeinfo", "params": [true, "192.168.0.201"] }' -H 'content-type: application/json;' http://127.0.0.1:8332/
+		#[rpc(name = "getaddednodeinfo")]
+		fn node_info(&self, bool, String) -> Result<(), Error>;
 	}
 }
