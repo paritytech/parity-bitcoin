59b30436c9d9ac99770c0b0cecdaf3e8105bc59c
NikVolf <nikvolf@gmail.com>
2016-12-15 11:38:56 +0100

    travis folding following echo

--- tools/bench.sh
+++ tools/bench.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo -en 'travis_fold:start:bench'
+echo 'Running bench from /benhcer'
 cargo run --manifest-path ./bencher/Cargo.toml --release
 echo -en 'travis_fold:end:bench'
--- tools/regtests.sh
+++ tools/regtests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo -en 'travis_fold:start:Regtests'
+echo 'Running regtests from ./tools/compare-tool/pull-tests-be0eef7.jar'
 
 ./target/release/pbtc --regtest --db-cache=192 &
 ! java -jar ./tools/compare-tool/pull-tests-be0eef7.jar /tmp/regtest-db 2>&1 | tee regtests-full.log | grep -E --color=auto 'org.bitcoinj.store.BlockStoreException\:|BitcoindComparisonTool.main\: ERROR|bitcoind sent us a block it already had, make sure bitcoind has no blocks!|java.lang.NullPointerException'
