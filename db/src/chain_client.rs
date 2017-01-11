08b09b3376fc496aa900e9a4f51f297aab1c7da9
NikVolf <nikvolf@gmail.com>
2017-01-11 13:33:04 +0300

    dramatically increase number of blocks, fix debug prints

--- db/src/chain_client.rs
+++ db/src/chain_client.rs
@@ -184,10 +184,10 @@ impl ChainClient {
 					TaskResult::Ok => {
 						// kick insert thread(s)
 						ChainClient::kick(&thread_more_insert);
-						println!("Kicked insert thread...");
+						trace!(target: "verificatino_queue", "Kicked insert thread...");
 					},
 					TaskResult::Wait => {
-						println!("Waiting for verify...");
+						trace!(target: "verification_queue", "Waiting for verify...");
 						// wait until kicked by fetch thread
 						let &(ref mutex, ref cvar) = &*thread_more_verify;
 						let mut kick = mutex.lock();
@@ -195,7 +195,7 @@ impl ChainClient {
 						while !*kick {
 							cvar.wait(&mut kick);
 						}
-						println!("Done waiting for verify...");
+						trace!(target: "verification_queue", "Done waiting for verify...");
 					},
 				}
 			}
@@ -222,10 +222,10 @@ impl ChainClient {
 					TaskResult::Ok => {
 						// kick verification thread(s)
 						ChainClient::kick(&thread_more_verify);
-						println!("Kicked verification thread...");
+						trace!(target: "verification_queue", "Kicked verification thread...");
 					},
 					TaskResult::Wait => {
-						println!("Waiting for fetch...");
+						trace!(target: "verification_queue", "Waiting for fetch...");
 						// wait until kicked by block push
 						let &(ref mutex, ref cvar) = &*thread_more_fetch;
 						let mut kick = mutex.lock();
@@ -233,7 +233,7 @@ impl ChainClient {
 						while !*kick {
 							cvar.wait(&mut kick);
 						}
-						println!("Done waiting for fetch...");
+						trace!(target: "verification_queue", "Done waiting for fetch...");
 					},
 				}
 			}
@@ -248,14 +248,14 @@ impl ChainClient {
 
 		thread::Builder::new().name("Insert thread".to_owned()).spawn(move || {
 			while !thread_stop.load(Ordering::SeqCst) {
-				println!("Inserting block...");
+				trace!(target: "verification_queue", "Inserting block...");
 				match thread_queue.insert_verified(&*thread_store) {
 					TaskResult::Ok => {
 						// continue with next block
-						println!("Inserted block");
+						trace!(target: "verification_queue", "Inserted block");
 					}
 					TaskResult::Wait => {
-						println!("Waiting for insert...");
+						trace!(target: "verification_queue", "Waiting for insert...");
 						// wait until kicked by verification thread
 						let &(ref mutex, ref cvar) = &*thread_more_insert;
 						let mut kick = mutex.lock();
@@ -263,7 +263,7 @@ impl ChainClient {
 						while !*kick {
 							cvar.wait(&mut kick);
 						}
-						println!("Done waiting for insert...");
+						trace!(target: "verification_queue", "Done waiting for insert...");
 					}
 				}
 			}
@@ -303,15 +303,15 @@ impl Drop for ChainClient {
 
 		ChainClient::kick(&self.more_fetch);
 		for thread in self.fetch_threads.drain(..) { thread.join().expect("Failed to join fetch thread"); }
-		println!("Finalized fetch threads");
+		trace!(target: "verification_queue", "Finalized fetch threads");
 
 		ChainClient::kick(&self.more_verify);
 		for thread in self.verification_threads.drain(..) { thread.join().expect("Failed to join verification thread"); }
-		println!("Finalized verification threads");
+		trace!(target: "verification_queue", "Finalized verification threads");
 
 		ChainClient::kick(&self.more_insert);
 		for thread in self.insert_threads.drain(..) { thread.join().expect("Failed to join insert thread"); }
-		println!("Finalized insert threads");
+		trace!(target: "verification_queue", "Finalized insert threads");
 
 		for thread in self.flush_threads.drain(..) { thread.join().expect("Failed to join flush thread"); }
 		self.store.flush();
@@ -373,7 +373,7 @@ mod tests {
 		let genesis = test_data::genesis();
 		let mut rolling_hash = genesis.hash();
 
-		for x in 0..5 {
+		for x in 0..100000 {
 			let mut coinbase_nonce = [0u8;8];
 			LittleEndian::write_u64(&mut coinbase_nonce[..], x as u64);
 			let next_block = test_data::block_builder()
