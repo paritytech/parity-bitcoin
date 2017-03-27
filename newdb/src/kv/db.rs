01d578e640e8267415e23654a1e8642454351992
debris <marek.kotewicz@gmail.com>
2017-03-27 17:29:42 +0700

    removed unused RwLock

--- newdb/src/kv/db.rs
+++ newdb/src/kv/db.rs
@@ -1,6 +1,6 @@
 use kv::{Location, Transaction, Value};
 
-pub trait KeyValueDatabase {
+pub trait KeyValueDatabase: Send + Sync {
 	fn write(&self, tx: Transaction) -> Result<(), String>;
 
 	fn get(&self, location: Location, key: &[u8]) -> Result<Option<Value>, String>;
