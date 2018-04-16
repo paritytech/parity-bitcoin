56b74c6c59962273e6346b079fd566d49e9918f7
5chdn <5chdn@users.noreply.github.com>
2018-04-16 12:36:53 +0200

    Fix regtest script

--- tools/regtests.sh
+++ tools/regtests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./target/release/pbtc --segwit --regtest --db-cache=192 &
+./target/release/pbtc --btc --regtest --db-cache=192 &
 ! java -jar ./tools/compare-tool/pull-tests-be0eef7.jar /tmp/regtest-db 2>&1 | tee regtests-full.log | grep -E --color=auto 'org.bitcoinj.store.BlockStoreException\:|BitcoindComparisonTool.main\: ERROR|bitcoind sent us a block it already had, make sure bitcoind has no blocks!|java.lang.NullPointerException'
 GREP_COLOR="01;32" grep 'BitcoindComparisonTool.main: Block "b1001" completed processing' regtests-full.log
 result=$?
