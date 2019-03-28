0db1dbd5ecde648d0220b58497c43d1b39926bdf
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2019-03-28 19:55:46 +0300

    docker fix and version script for snapcraft

--- tools/docker_build.sh
+++ tools/docker_build.sh
@@ -4,16 +4,16 @@
 set -e # fail on any error
 set -u # treat unset variables as error
 
-if [ "$CI_COMMIT_REF_NAME" == "master" ];
-	then export DOCKER_BUILD_TAG="latest}";
-	else export DOCKER_BUILD_TAG=$CI_COMMIT_REF_NAME;
-fi
+#if [ "$CI_COMMIT_REF_NAME" == "master" ];
+#	then export DOCKER_BUILD_TAG="latest}";
+#	else export DOCKER_BUILD_TAG=$CI_COMMIT_REF_NAME;
+#fi
 docker login -u $Docker_Hub_User_Parity -p $Docker_Hub_Pass_Parity
 
 echo "__________Docker TAG__________"
 echo $DOCKER_BUILD_TAG
 
 echo "__________Docker build and push__________"
-docker build --no-cache=true --tag parity/pbtc-ubuntu:$DOCKER_BUILD_TAG -f docker/hub/hub/Dockerfile .
-docker push parity/pbtc-ubuntu:$DOCKER_BUILD_TAG
+docker build --no-cache=true --tag parity/pbtc-ubuntu:latest -f docker/hub/hub/Dockerfile .
+docker push parity/pbtc-ubuntu:latest
 docker logout
--- tools/ver.sh
+++ tools/ver.sh
@@ -3,5 +3,5 @@
 set -e # fail on any error
 set -u # treat unset variables as error
 rm -rf deb
-#get current version
+#get current version for snap
 grep -m 1 version Cargo.toml | awk '{print $3}' | tr -d '"' | tr -d "\n"
