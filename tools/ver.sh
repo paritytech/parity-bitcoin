6c7192ef548068d033e97219e7338f442937af92
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2019-03-28 22:40:52 +0300

    new snapcraft template

--- tools/ver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e # fail on any error
-set -u # treat unset variables as error
-rm -rf deb
-#get current version for snap
-grep -m 1 version Cargo.toml | awk '{print $3}' | tr -d '"' | tr -d "\n"
